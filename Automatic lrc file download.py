12207e5909717afeb4f5e0edb8a5c59413daca47
Upokharel56 <155223739+Upokharel56@users.noreply.github.com>
2024-02-23 22:25:24 +0545

    Update Automatic lrc file download.py
    This is smaller and cleaner version of previous code that works completely same as it was before.

--- Automatic lrc file download.py
+++ Automatic lrc file download.py
@@ -1,151 +1,97 @@
 from bs4 import BeautifulSoup
-import requests, webbrowser,re
-import os
+import requests,re
 from pathlib import Path
 import os
 
-filename ="songList.txt"
-captchaTry=0
-
-def check_captcha(searchLink):
-			
-	response=requests.get(searchLink)		 	
-	soup=BeautifulSoup(response.text,"html.parser")		
-	code=soup.prettify()	
-	captcha= soup.find_all("form",{"id":"captcha-form"})
-			
-	if(captcha):
-		print("Captcha found Oh noo. anandon mission fastt.")
-		return True
-	
-	else:
-		return False
 
 
+captchaTry = 0
 
-
+def check_captcha_form(soup):
+    captcha = soup.find_all("form", {"id": "captcha-form"})
+    if captcha:
+        return True
+    else:
+        return False
 
 def get_first_link(searchLink):
-	response=requests.get(searchLink)
-	 	
-	soup=BeautifulSoup(response.text,"html.parser")
-	
-	code=soup.prettify()
-	
-	elements = soup.find_all("div", class_="egMi0")
-	try:
-		first_res=elements[0]			
-		#print(first_res)
-		print("\n\n")		
-		links = first_res.find_all("a")
-		for link in links:
-		    link_url = link["href"]
-		#print(code)
-		match = re.search(r'https://[^\&]+', link_url)
+    response = requests.get(searchLink)
+    soup = BeautifulSoup(response.text, "html.parser")
 
-	except IndexError:
-		print(f"List out of errors : \n")
-		print(elements)
-		match=False
-				
-	# Check if a match was found
-	if match:
-	    link = match.group()
-	    #print(link)
-	    return link
-	else:
-	    
-	    thereIsCaptcha = check_captcha(searchLink)
-	    global captchaTry
-	    
-	    if(thereIsCaptcha):
-	    	print("Captcha Block Found Will run again and If again Found terminate the program. ")
-	    	
-	    	captchaTry=captchaTry +1
-	    	
-	    	if(captchaTry > 2):
-	    		print("\n Captcha Is blocking Scraping Please Try after Some time Or use a proxy Network of adress. ")
-	    		exit()
-	    	return "Captcha_Error"
-	    else:
-	    	print("No link found ")
-	    
-	    return False
-	  
-	              
+    elements = soup.find_all("div", class_="egMi0")
+    try:
+        first_res = elements[0]
+        links = first_res.find_all("a")
+        link_url = links[0]["href"]
+        match = re.search(r'https://[^\&]+', link_url)
+    except IndexError:
+        print("List out of href links:")
+        print(elements)
+        match = False
 
+    if match:
+        link = match.group()
+        return link
+    else:
+        thereIsCaptcha = check_captcha_form(soup)
+        global captchaTry
 
+        if thereIsCaptcha:
+            print("Captcha Block Found. We will try once again and if found again, the program will be terminated.")
+            captchaTry += 1
 
+            if captchaTry > 2:
+                print("Captcha is blocking us from getting the link. Please try again later or use a proxy network.")
+                exit()
+            return "Captcha_Error"
 
+        return False
 
 def get_lyrics(lrc_link):
-	response=requests.get(lrc_link)
-		 	
-	soup=BeautifulSoup(response.text,"html.parser")
-	
-	code=soup.prettify()
-	
-	elements = soup.find_all("div", class_="lyrics_details")
-	
-	try:
-		#print("Lyrics is \n\n")
-		lyrics=elements[0].text
-		return lyrics
-	except IndexError:
-		print(f"Complex file Name :  ")
-		print(elements)
-		return False
-				
+    response = requests.get(lrc_link)
+    soup = BeautifulSoup(response.text, "html.parser")
 
-		
-				
-						
-										
-def write_to_file(lyrics,lrcFileName):
-	lrc_extension=lrcFileName.replace(".mp3",".lrc")
-	# Create a Path object for the new file.
-	file_path = Path(f"my_LRCS/{lrc_extension}")
-	
-	# Create the folder if it does not exist.
-	file_path.parent.mkdir(parents=True, exist_ok=True)
-	
-	# Open the file for writing using the 'x' mode.
-	try:
-		with file_path.open("x") as f:
-		    f.write(lyrics)
-		print(f"Sucesdfully written:  {lrcFileName} ")
-	except FileExistsError:
-		print(f"{lrcFileName} file already exists")		
+    elements = soup.find_all("div", class_="lyrics_details")
 
+    try:
+        lyrics = elements[0].text
+        return lyrics
+    except IndexError:
+        print("Link may have an error or our website doesn't have your desired lyrics:")
+        print(elements)
+        return False
 
+def write_to_file(lyrics, lrcFileName):
+    lrc_extension = lrcFileName.replace(".mp3", ".lrc")
+    file_path = Path(f"my_LRCS/{lrc_extension}")
 
+    file_path.parent.mkdir(parents=True, exist_ok=True)
+    
+    try:
+        with file_path.open("x") as f:
+            f.write(lyrics)
+        print(f"Successfully written: {lrcFileName}")
+    except FileExistsError:
+        print(f"{lrcFileName} file already exists. We didn't override the file. If you want to override it, please specify.")
+
+songTitles = ['Coldplay - Sparks']
 
 def main():
-	with open(filename,"r") as f:
-		line=f.readline()
-		while line != "":				
-			#print(line)
-	
-	    # Read the next line from the file.		
-			search_query =f"{line} lrc megalobiz"
-			
-			searchLink=f"""https://www.google.com/search?q={search_query}"""
-								
-			lrc_link= get_first_link(searchLink)
-			
-			if(lrc_link and lrc_link !="Captcha_Error"):
-				lyrics=get_lyrics(lrc_link)
-				if(lyrics):
-					write_to_file(lyrics,line)
-					
-					
-				#print(lyrics)
-			elif(lrc_link =="Captcha_Error"):
-				pass	
-				
-			elif(not lrc_link):
-				print(f"No link found for: {line}")
-			line = f.readline()
+    for songName in songTitles:
+        line = songName.replace("(Audio)", "").replace("(Official Video)", "").replace("(Official Lyric Video)", "")
 
+        if line == "":
+            print("Song Name is Empty!")
+            return
+
+        searchLink = f"https://www.google.com/search?q={line} lrc megalobiz"
+        lrc_link = get_first_link(searchLink)
+
+        if lrc_link and lrc_link != "Captcha_Error":
+            lyrics = get_lyrics(lrc_link)
+            if lyrics:
+                write_to_file(lyrics, songName)
+        elif not lrc_link:
+            print(f"No link found for: {line}")
 
 main()
